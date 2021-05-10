2f4a817f06346528f807537863ded0dfad448ae6
Jeremy Soller <jeremy@system76.com>
2021-05-09 20:57:25 -0600

    Update jobserver and work around warnings when building libstd for redox

--- src/libstd/lib.rs
+++ src/libstd/lib.rs
@@ -215,6 +215,7 @@
 #![deny(intra_doc_link_resolution_failure)] // rustdoc is run without -D warnings
 #![allow(explicit_outlives_requirements)]
 #![allow(unused_lifetimes)]
+#![allow(unused_unsafe)] // For redox libc issues
 // Tell the compiler to link to either panic_abort or panic_unwind
 #![needs_panic_runtime]
 // std may use features in a platform-specific way

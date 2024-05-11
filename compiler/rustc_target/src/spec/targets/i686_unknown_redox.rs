cd4b8b84bee94c91ecfbab6d0e28aebdd2b71816
Jeremy Soller <jackpot51@gmail.com>
2024-05-10 21:16:01 -0600

    Fix location of i686 redox target spec

--- compiler/rustc_target/src/spec/targets/i686_unknown_redox.rs
+++ compiler/rustc_target/src/spec/targets/i686_unknown_redox.rs
@@ -1,8 +1,9 @@
-use crate::spec::{Cc, LinkerFlavor, Lld, StackProbeType, Target};
+use crate::spec::{base, Cc, LinkerFlavor, Lld, StackProbeType, Target};
 
 pub fn target() -> Target {
-    let mut base = super::redox_base::opts();
+    let mut base = base::redox::opts();
     base.cpu = "pentiumpro".into();
+    base.plt_by_default = false;
     base.max_atomic_width = Some(64);
     base.add_pre_link_args(LinkerFlavor::Gnu(Cc::Yes, Lld::No), &["-m32"]);
     // don't use probe-stack=inline-asm until rust#83139 and rust#84667 are resolved
@@ -10,6 +11,12 @@ pub fn target() -> Target {
 
     Target {
         llvm_target: "i686-unknown-redox".into(),
+        metadata: crate::spec::TargetMetadata {
+            description: None,
+            tier: None,
+            host_tools: None,
+            std: None,
+        },
         pointer_width: 32,
         data_layout: "e-m:e-p:32:32-p270:32:32-p271:32:32-p272:64:64-f64:32:64-f80:32-n8:16:32-S128"
             .into(),

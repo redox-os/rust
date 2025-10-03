4ea89b7ceb2e50a78fa24f9ba1c7e4f22a26b00d
Jeremy Soller <jackpot51@gmail.com>
2025-10-03 09:12:23 -0600

    Add i686-unknown-redox and riscv64gc-unknown-redox

--- compiler/rustc_target/src/spec/mod.rs
+++ compiler/rustc_target/src/spec/mod.rs
@@ -1553,7 +1553,9 @@ supported_targets! {
 
     ("aarch64-unknown-redox", aarch64_unknown_redox),
     ("i586-unknown-redox", i586_unknown_redox),
+    ("i686-unknown-redox", i686_unknown_redox),
     ("x86_64-unknown-redox", x86_64_unknown_redox),
+    ("riscv64gc-unknown-redox", riscv64gc_unknown_redox),
 
     ("x86_64-unknown-managarm-mlibc", x86_64_unknown_managarm_mlibc),
     ("aarch64-unknown-managarm-mlibc", aarch64_unknown_managarm_mlibc),
--- src/bootstrap/src/core/sanity.rs
+++ src/bootstrap/src/core/sanity.rs
@@ -34,6 +34,8 @@ pub struct Finder {
 // Targets can be removed from this list once they are present in the stage0 compiler (usually by updating the beta compiler of the bootstrap).
 const STAGE0_MISSING_TARGETS: &[&str] = &[
     // just a dummy comment so the list doesn't get onelined
+    "i686-unknown-redox",
+    "riscv64gc-unknown-redox",
     "x86_64-unknown-motor",
 ];
 
